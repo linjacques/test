5cacffffee53e1aa1dfc7a67cd32208cc434922d
linjacques <linjacques463@gmail.com>
2025-02-06 10:13:39 +0100

    ajout d'un test unitaire

---
+++ tests/fizzbuzzTest.py
@@ -0,0 +1,19 @@
+import pytest
+from fizzbuzz import fizzbuzz
+
+def test_fizzbuzz_divisible_by_3():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(6) == "Fizz"
+
+def test_fizzbuzz_divisible_by_5():
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(10) == "Buzz"
+
+def test_fizzbuzz_divisible_by_3_and_5():
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+
+def test_fizzbuzz_not_divisible_by_3_or_5():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(7) == 7
+    assert fizzbuzz(11) == 11
