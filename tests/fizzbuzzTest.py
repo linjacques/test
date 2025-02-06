dc9b5b2e18b2a5e5973093c359946cf639b06364
linjacques <linjacques463@gmail.com>
2025-02-06 11:15:44 +0100

    retour sur le nommage des fonctions sur pytest, fix du bug des tests2

--- tests/fizzbuzzTest.py
+++ tests/fizzbuzzTest.py
@@ -1,19 +1,19 @@
 import pytest
 from fizzbuzz import fizzbuzz
 
-def est_divisible_par_3():
+def test_est_divisible_par_3():
     assert fizzbuzz(3) == "Fizz"
     assert fizzbuzz(6) == "Fizz"
 
-def est_divisible_par_5():
+def test_est_divisible_par_5():
     assert fizzbuzz(5) == "Buzz"
     assert fizzbuzz(10) == "Buzz"
 
-def est_divisible_par_3_et_5():
+def test_est_divisible_par_3_et_5():
     assert fizzbuzz(15) == "FizzBuzz"
     assert fizzbuzz(30) == "FizzBuzz"
 
-def est_divisible_par_3_ou_5():
+def test_est_divisible_par_3_ou_5():
     assert fizzbuzz(1) == 1
     assert fizzbuzz(7) == 7
     assert fizzbuzz(11) == 11
