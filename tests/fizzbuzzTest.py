4753381b3e20ce33834e5b7221dd532fc257bc19
linjacques <linjacques463@gmail.com>
2025-02-06 10:24:09 +0100

    renommage des fonctions

--- tests/fizzbuzzTest.py
+++ tests/fizzbuzzTest.py
@@ -1,19 +1,19 @@
 import pytest
 from fizzbuzz import fizzbuzz
 
-def test_fizzbuzz_divisible_by_3():
+def est_divisible_par_3():
     assert fizzbuzz(3) == "Fizz"
     assert fizzbuzz(6) == "Fizz"
 
-def test_fizzbuzz_divisible_by_5():
+def est_divisible_par_5():
     assert fizzbuzz(5) == "Buzz"
     assert fizzbuzz(10) == "Buzz"
 
-def test_fizzbuzz_divisible_by_3_and_5():
+def est_divisible_par_3_et_5():
     assert fizzbuzz(15) == "FizzBuzz"
     assert fizzbuzz(30) == "FizzBuzz"
 
-def test_fizzbuzz_not_divisible_by_3_or_5():
+def est_divisible_par_3_ou_5():
     assert fizzbuzz(1) == 1
     assert fizzbuzz(7) == 7
     assert fizzbuzz(11) == 11
